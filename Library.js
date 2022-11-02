92a418b6827bace6cdc9a0aa989bdfe64e39d16c
Henry Dufour <henrydufourdiscord@gmail.com>
2022-11-02 09:07:05 -0400

    all my progress.

--- Library.js
+++ Library.js
@@ -3,11 +3,12 @@ const express = require("express")
 const app = express()
 
 const { MongoClient, ObjectId } = require("mongodb")
+// const clientStuff = require("./client.js")
 
 const PORT = process.env.PORT || 4000
 
 class Library {
-    constructor(dbUrl, dbName, collName) {
+    constructor(dbUrl , dbName, collName) {
         this.dbUrl = dbUrl;
         this.dbName = dbName;
         this.collName = collName;
@@ -24,6 +25,24 @@ class Library {
     async test() {
         const client = await this.client()
         client.close()
+    }
+
+    async collection() {
+        const client = await this.client();
+        const db = client.db(this.dbName);
+        const collection = db.collection(this.collName);
+        return collection;
+    }
+
+    async allBooks(){
+        let collection = await this.collection()
+        return collection.find({}).toArray()
+    }
+
+    async findOneBook(id) {
+        let docId = ObjectId(id)
+        let collection = await this.collection()
+        return collection.find(docId).toArray()
     }
 
 }
--- client.js
+++ client.js
@@ -1,4 +1,15 @@
+// const router = require("express").Router()
 const Library = require('./Library.js')
 
 const collection = new Library("mongodb://127.0.0.1:27017", "library", "books")
+
 collection.test()
+
+async function test() {
+    let allBooks = await collection.allBooks()
+    let findOneBook = await collection.findOneBook("63618487df6886b7b3d7e250")
+    console.log(findOneBook)
+}
+
+test()
+// module.exports = router
